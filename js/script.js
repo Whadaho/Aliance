47db4db7cf9672beaf0ca7bb0f66ce2112060991
Whadaho <surkovlev660@gmail.com>
2026-09-07 18:33:00 +0400

    fix: navbar js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ const navbar = document.querySelector(".navbar")
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light")
-  } else { navbar.classList.remove("navbar-light")  
-
+  } else {navbar.classList.remove("navbar-light")
+  
   }
   
+  
+
 })
